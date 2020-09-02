f38c3bd5a5a5412a95b2a631c6c57e819dd6a54c
haripalbaluja <haripalbaluja.208@gmail.com>
2020-09-02 15:42:34 +0530

    maybee

--- backend/settings.py
+++ backend/settings.py
@@ -132,7 +132,8 @@ STATICFILES_DIRS = (
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 
 # CORS_ORIGIN_WHITELIST = (
